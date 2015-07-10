13d03112501fd0570ec0b04e233c260c72e0d476
Yosuke ONOUE <onoue@likr-lab.com>
2015-07-10 15:47:43 +0900

    Separate MainComponent

--- src/index.jsx
+++ src/index.jsx
@@ -1,50 +1,5 @@
-/* global document,fetch */
+/* global document*/
 import React from 'react';
-import 'whatwg-fetch';
-import GraphStore from './graph-store';
-import {loadGraph} from './graph-actions';
-import NetworkDiagram from './network-diagram';
-
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      graph: GraphStore.getGraph()
-    };
-  }
-
-  componentDidMount() {
-    fetch('data/graph.json')
-      .then((response) => response.json())
-      .then(loadGraph);
-    GraphStore.addChangeListener(this.onChangeGraph.bind(this));
-  }
-
-  componentWillUnmount() {
-    GraphStore.removeChangeListener(this.onChangeGraph.bind(this));
-  }
-
-  onChangeGraph() {
-    this.setState({
-      graph: GraphStore.getGraph()
-    });
-  }
-
-  render() {
-    const wrapperStyle = {
-      position: 'absolute',
-      top: 0,
-      bottom: '10px',
-      left: 0,
-      right: 0
-    };
-    return (
-      <div style={wrapperStyle}>
-        <NetworkDiagram graph={this.state.graph}/>
-      </div>
-    );
-  }
-}
+import Main from './main';
 
 React.render(<Main/>, document.getElementById('content'));
