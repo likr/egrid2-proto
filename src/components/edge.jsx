4fbddbd1711d48a54007fc23de0ef20f61fc3962
Yosuke ONOUE <onoue@likr-lab.com>
2015-07-14 20:43:09 +0900

    Improve performance of edge calculation

--- src/components/edge.jsx
+++ src/components/edge.jsx
@@ -1,27 +1,42 @@
 import React from 'react';
 
-const startFrom = ([x, y]) => {
+const startFrom = (x, y) => {
   return `M${x} ${y}`;
 };
 
-const lineTo = ([x, y]) => {
+const lineTo = (x, y) => {
   return ` L${x} ${y}`;
 };
 
-const curveTo = ([x1, y1], [x2, y2], ltor) => {
+const curveTo = (x1, y1, x2, y2) => {
   const dx = x2 - x1,
-        dy = y2 - y1;
-  return ltor
-    ? ` q${dx / 4} ${0},${dx / 2} ${dy / 2} q${dx / 4} ${dy / 2},${dx / 2} ${dy / 2}`
-    : ` q${0} ${dy / 4},${dx / 2} ${dy / 2} q${dx / 2} ${dy / 4},${dx / 2} ${dy / 2}`;
+        dy = y2 - y1,
+        dx2 = dx / 2,
+        dy2 = dy / 2,
+        dx4 = dx / 4;
+  return ` q${dx4} 0,${dx2} ${dy2} q${dx4} ${dy2},${dx2} ${dy2}`;
 };
 
-const svgPath = (points, ltor) => {
-  let d = `${startFrom(points[0])}${lineTo(points[1])}`;
-  for (let i = 3; i < points.length; i += 2) {
-    d += curveTo(points[i - 2], points[i - 1], ltor) + lineTo(points[i]);
-  }
-  return d;
+const interpolate = (a, a0, t) => {
+  return (a - a0) * t + a0;
+};
+
+const svgPath = (points, points0, t) => {
+  const x0 = interpolate(points[0][0], points0[0][0], t),
+        y0 = interpolate(points[0][1], points0[0][1], t),
+        x1 = interpolate(points[1][0], points0[1][0], t),
+        y1 = interpolate(points[1][1], points0[1][1], t),
+        x2 = interpolate(points[2][0], points0[2][0], t),
+        y2 = interpolate(points[2][1], points0[2][1], t),
+        x3 = interpolate(points[3][0], points0[3][0], t),
+        y3 = interpolate(points[3][1], points0[3][1], t),
+        x4 = interpolate(points[4][0], points0[4][0], t),
+        y4 = interpolate(points[4][1], points0[4][1], t),
+        x5 = interpolate(points[5][0], points0[5][0], t),
+        y5 = interpolate(points[5][1], points0[5][1], t);
+  return (startFrom(x0, y0) + lineTo(x1, y1)
+          + curveTo(x1, y1, x2, y2) + lineTo(x3, y3)
+          + curveTo(x3, y3, x4, y4) + lineTo(x5, y5));
 };
 
 const edgeColor = (upper, lower) => {
@@ -66,16 +81,11 @@ class Edge extends React.Component {
   }
 
   render() {
-    const t = this.props.t;
-    const points = this.props.points.map((point, i) => {
-      const [x, y] = point,
-            [x0, y0] = this.props.points0[i];
-      return [(x - x0) * t + x0, (y - y0) * t + y0];
-    });
+    const {points, points0, t} = this.props;
     return (
       <g className="edge">
         <path
-            d={svgPath(points, true)}
+            d={svgPath(points, points0, t)}
             fill="none"
             stroke={edgeColor(this.props.upper, this.props.lower)}
             strokeDasharray={this.props.reversed ? 5 : 'none'}
